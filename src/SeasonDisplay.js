b6294489b7a483e611435c3fc4382f88cf9f899e
Randy <rgoldshz@gmail.com>
2021-02-18 15:49:33 -0600

    added error message

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -5,18 +5,24 @@ class SeasonDisplay extends React.Component {
     constructor(props) {
         super(props)
         
-        this.state = { lat: null};
+        this.state = { lat: null, errorMessage: ''};
     }
 
     render() {
         navigator.geolocation.getCurrentPosition( 
             position =>  { this.setState({ lat: position.coords.latitude});
         },
-            err => console.log(err)
+            err => { this.setState({ errorMessage: err.message});
+            }
         );
 
         return(
-            <div>Latitude: {this.state.lat}</div>
+            <div>
+                Latitude: {this.state.lat}
+                <br />
+                Error: {this.state.errorMessage}
+            </div>
+            
         );
     }
 }
